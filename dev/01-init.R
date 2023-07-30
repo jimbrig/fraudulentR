cb99bf34b295383ef5e51663bf8ddb067e68083b
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2023-07-30 17:53:11 -0400

    Initial commit

---
+++ dev/01-init.R
@@ -0,0 +1,16 @@
+
+#  ------------------------------------------------------------------------
+#
+# Title : Initialize R Package Script
+#    By : Jimmy Briggs
+#  Date : 2023-07-30
+#
+#  ------------------------------------------------------------------------
+
+
+# library devt packages ---------------------------------------------------
+
+require(usethis)
+require(devtools)
+
+usethis::create_package(getwd())
